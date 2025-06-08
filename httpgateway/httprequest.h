f28031720f4cd726a392ee806c582b1d0e586625
Serg6464 <sinyakovs@inbox.ru>
2025-06-08 23:30:28 +0400

    рефакторинг

--- httpgateway/httprequest.h
+++ httpgateway/httprequest.h
@@ -10,24 +10,12 @@ private:
  std::shared_ptr<http::request<http::string_body>> request_;
 
 public:
-    HttpRequest(const std::string& raw_request);
-    HttpRequest(std::shared_ptr<http::request<http::string_body>> request)
-        :request_(request)
-    {
-
-    };
-    static std::shared_ptr<HttpRequest> Create()
-    {
-        std::shared_ptr<http::request<http::string_body>> req = std::make_shared<http::request<http::string_body>>();
-        return std::make_shared<HttpRequest>(req);
-    };
+    HttpRequest(std::shared_ptr<http::request<http::string_body>> request);;
+    static std::shared_ptr<HttpRequest> Create();;
 
     // IHttpRequest interface
 public:
-    std::shared_ptr<http::request<http::string_body>> getRequest()
-    {
-        return request_;
-    };
+    std::shared_ptr<http::request<http::string_body>> getRequest();;
 
 };
 
